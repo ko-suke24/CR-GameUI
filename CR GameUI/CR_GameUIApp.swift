077aa9b47b753bf5b147eb625f05ddef63e7a54f
ko-suke24 <kosuke0322@gmail.com>
2021-06-09 15:34:09 +0900

    Initial Commit

---
+++ CR GameUI/CR_GameUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CR_GameUIApp.swift
+//  CR GameUI
+//
+//  Created by 佐藤航輔 on 2021/06/09.
+//
+
+import SwiftUI
+
+@main
+struct CR_GameUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CR GameUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CR GameUI
+//
+//  Created by 佐藤航輔 on 2021/06/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
